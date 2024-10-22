800608ce993c045112d8ebb56e7efe39dcd2f10d
Alexandre Freire <alexandre.com9@gmail.com>
2024-10-22 18:57:57 -0400

    abrir pagina welcome apos instalacao

--- codigo/background.js
+++ codigo/background.js
@@ -2,6 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.create({ url: "https://web.whatsapp.com" }, (tab) => {
         chrome.storage.local.set({ blurEnabled: true }, () => {
             console.log("Blur enabled set to true.");
+            chrome.tabs.create({ url: "https://privacidade-whatsapp-web.vercel.app/welcome" });
         });
     });
 });
