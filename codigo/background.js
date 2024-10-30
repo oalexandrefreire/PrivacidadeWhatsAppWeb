62656ffeeb8ea77721253420b817dfbe30dd5ed3
Alexandre Freire <alexandre.com9@gmail.com>
2024-10-30 17:00:46 -0400

    correcao bug e atualizacao landing page
    Ao iniciar o navegador a extensão estava abrindo o WhatsApp

--- codigo/background.js
+++ codigo/background.js
@@ -1,26 +1,33 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.create({ url: "https://web.whatsapp.com" }, (tab) => {
-        const defaultValues = {
-            blurNomesEnabled: false,
-            blurMensagensEnabled: false,
-            blurPreviewEnabled: false,
-            blurEntradaEnabled: false,
-            blurImagensEnabled: false
-        };
+    const defaultValues = {
+        blurNomesEnabled: false,
+        blurMensagensEnabled: false,
+        blurPreviewEnabled: false,
+        blurEntradaEnabled: false,
+        blurImagensEnabled: false
+    };
 
-        chrome.storage.local.get(Object.keys(defaultValues).concat('blurEnabled'), (storedValues) => {
-            if (storedValues.blurEnabled === undefined) {
-                storedValues.blurEnabled = true; 
+    chrome.storage.local.get(Object.keys(defaultValues).concat('blurEnabled'), (storedValues) => {
+        const allBlurOptionsSet = Object.keys(defaultValues).every(key => storedValues[key] !== undefined);
+
+        if (allBlurOptionsSet) {
+            console.log("As opções de blur já estão preenchidas. A extensão já foi instalada.");
+            return; 
+        }
+
+        if (storedValues.blurEnabled === undefined) {
+            storedValues.blurEnabled = true; 
+        }
+
+        for (const key in defaultValues) {
+            if (storedValues[key] === undefined) {
+                storedValues[key] = defaultValues[key];
             }
+        }
 
-            for (const key in defaultValues) {
-                if (storedValues[key] === undefined) {
-                    storedValues[key] = defaultValues[key];
-                }
-            }
-
-            chrome.storage.local.set(storedValues, () => {
-                console.log("Storage initialized with default values.");
+        chrome.storage.local.set(storedValues, () => {
+            console.log("Storage initialized with default values.");
+            chrome.tabs.create({ url: "https://web.whatsapp.com" }, (tab) => {
                 chrome.tabs.create({ url: "https://privacidade-whatsapp-web.vercel.app/welcome.html" });
             });
         });
